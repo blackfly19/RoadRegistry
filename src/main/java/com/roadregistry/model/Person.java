fa6c549db1b233194b93ff17cd59820e0228546b
Ankit Chawla <ankitc226@gmail.com>
2025-05-23 11:40:56 +1000

    Added attributes for the person class

--- src/main/java/com/roadregistry/model/Person.java
+++ src/main/java/com/roadregistry/model/Person.java
@@ -1,5 +1,17 @@
 package com.roadregistry.model;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class Person {
+
+    private String personID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String birthDate;
+    private HashMap<Date, Integer> demeritPoints;
+    private boolean isSuspended;
+
 
 }
